884daf13762b533f4f2392d383b38c1b9d1a0ffd
r14f <57841815+r14f@users.noreply.github.com>
2025-06-01 16:30:31 -0500

    Update launch.sh to 3.2

--- launch.sh
+++ launch.sh
@@ -3,7 +3,7 @@
 set -x
 
 NEOFORGE_VERSION=21.1.174
-SERVER_VERSION=3.1
+SERVER_VERSION=3.2
 cd /data
 
 if ! [[ "$EULA" = "false" ]]; then
@@ -15,7 +15,7 @@ fi
 
 if ! [[ -f "Server-Files-$SERVER_VERSION.zip" ]]; then
     rm -fr config defaultconfigs kubejs mods packmenu Server-Files-* neoforge*
-    curl -Lo "Server-Files-$SERVER_VERSION.zip" "https://mediafilez.forgecdn.net/files/6604/855/ServerFiles-$SERVER_VERSION.zip" || exit 9
+    curl -Lo "Server-Files-$SERVER_VERSION.zip" "https://mediafilez.forgecdn.net/files/6606/334/ServerFiles-$SERVER_VERSION.zip" || exit 9
     unzip -u -o "Server-Files-$SERVER_VERSION.zip" -d /data
     DIR_TEST="ServerFiles-$SERVER_VERSION"
     if [[ $(find . -type d -maxdepth 1 | wc -l) -gt 1 ]]; then
